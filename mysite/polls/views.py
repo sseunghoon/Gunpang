641f9af194f653f17baaf90d129b9aacf53ef338
sseunghoon <sseunghun99@naver.com>
2021-04-30 13:46:16 +0000

    results.html

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,11 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+from django.urls import reverse
 
-from .models import Question
+
+from .models import Choice, Question
 
 
 def index(request):
@@ -32,10 +34,41 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 
+# from django.shortcuts import get_object_or_404
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+
+# get_object_or_404 처럼 동작하는 get_list_or_404 함수도 존재함
+# get() 대신 filter()를 쓰는 것만 다름.(리스트가 비어있을 경우 Http404)
+
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # question voting form을 다시 보여줌
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # POST data가 잘 처리되었으면 언제나 HttpResponseRedirect를 줘서
+        # 유저가 뒤로 가기 버튼을 눌렀을 때 2번 전송되는 것을 방지함
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+# request.POST의 값은 항상 문자열들임.
+# reverse() 함수는 뷰 함수에서 URL을 하드코딩하지 않도록 도와줌.
+
+
+
+
+
+
