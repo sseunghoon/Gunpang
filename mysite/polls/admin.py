dd039b561256b7a108f20994e25c5073bb08d72f
sseunghoon <sseunghun99@naver.com>
2021-05-01 12:51:22 +0000

    End

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+# Inline related objects가 공간을 잡아먹어 StackedInline을 TabularInline으로 교체
+# TabularInline은 좀 더 조밀한 테이블 기반 형식으로 표시
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}),
+                 ('Date infomation', {'fields': ['pub_date'], 'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+
